59e19d1abe0403c6dd8396cb1324d9f79682589a
mich-hales <michellehales93@gmail.com>
2022-08-13 09:54:45 -0600

    tried to add save text input with save button

--- Develop/script.js
+++ Develop/script.js
@@ -9,4 +9,61 @@ function todaysDate() {
     }, 1000);
   }
 
+  // call function
 todaysDate();
+
+
+
+// function to color code past, present and future hours
+function colorCode() {
+  
+    $('.form-control').each(function() {
+        // selecting id value to match up timeline
+        var timeLine = parseInt($('this').attr('id'));
+        // current hour 
+        var currentHour = moment().hours();
+        // logs military time
+        console.log(currentHour);
+        console.log(timeLine);
+
+        if (currentHour > timeLine) {
+            $('this').addClass('past');
+        } else if (currentHour < timeLine){
+            $('this').addClass('future');
+        } else {
+            $('this').addClass('present');
+        }
+    })
+}
+
+colorCode();
+
+
+
+
+// testing local storage
+// var textInput = JSON.parse()
+
+
+
+var userInput;
+var hourText
+
+// function to save text input to local storage when save button is clicked
+// (jquery on click for event listener)
+$('.save').on('click', function() {
+    // selects all siblings of the button clicked and gets value of text input from class form-control
+    userInput = $('this').siblings('.form-control').val();
+    console.log(userInput);
+    hourText = $('this').siblings('.input-group-prepend').text();
+    console.log(hourText);
+    // stringify
+    var objString = JSON.stringify(userInput);
+    localStorage.setItem(hourText, objString);
+
+    console.log(objString);
+});
+
+
+
+
